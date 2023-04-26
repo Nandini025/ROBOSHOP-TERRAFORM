ec1f5f73fe0698e6041b99ffd7acc3b554169294
Nandini025 <karlapudinandini025@gmail.com>
2023-04-26 18:13:35 +0530

    added data aws_ami code to see wether we are getting ami or not

--- servers.tf
+++ servers.tf
@@ -6,6 +6,18 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+data "aws_ami" "centos" {
+  most_recent = true
+  name_regex = "Centos-8-Devops-Practice "
+  owners      = ["073915655652"]
+}
+output ami{
+  value= data.aws_ami.centos.image_id
+}
+
+
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
